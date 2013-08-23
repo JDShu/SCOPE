71fe68d46167b9d621a03ac7083399225e83ec3b
Hans Lo <hansshulo@gmail.com>
2013-08-23 19:53:36 -0400

    Default to Debug=False

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ import site
 ASKBOT_ROOT = os.path.abspath(os.path.dirname(askbot.__file__))
 site.addsitedir(os.path.join(ASKBOT_ROOT, 'deps'))
 
-DEBUG = True#set to True to enable debugging
+DEBUG = False #set to True to enable debugging
 TEMPLATE_DEBUG = False#keep false when debugging jinja2 templates
 INTERNAL_IPS = ('127.0.0.1',)
 
